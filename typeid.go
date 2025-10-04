f99dab184e3adde65bf818f538a9cab090d3351e
Arkam Fahri <92285161+pixelbend@users.noreply.github.com>
2025-10-04 20:16:10 +0530

    chore: enforce lowercase in type names and ULIDs
    Update Make and Parse functions to handle lowercase ULIDs and enforce lowercase type names.

--- typeid.go
+++ typeid.go
@@ -37,28 +37,28 @@ func Make(typeName string) (TypeId, error) {
 	if err := validateTypeName(typeName); err != nil {
 		return "", err
 	}
-
-	return TypeId(fmt.Sprintf("%s_%s", typeName, ulid.Make().String())), nil
+	
+	return TypeId(fmt.Sprintf("%s_%s", typeName, strings.ToLower(ulid.Make().String()))), nil
 }
 
 func Parse(typeName string, typeId string) (TypeId, error) {
 	if err := validateTypeName(typeName); err != nil {
 		return "", err
 	}
-
+	
 	typeIdParts := strings.SplitN(typeId, "_", 2)
 	if len(typeIdParts) != 2 {
 		return "", fmt.Errorf("type id must contain exactly one underscore separating type and ulid")
 	}
-
+	
 	if typeIdParts[0] != typeName {
 		return "", fmt.Errorf("type id prefix '%s' does not match expected type name '%s'", typeIdParts[0], typeName)
 	}
-
-	if _, err := ulid.Parse(typeIdParts[1]); err != nil {
+	
+	if _, err := ulid.Parse(strings.ToUpper(typeIdParts[1])); err != nil {
 		return "", fmt.Errorf("invalid ulid '%s': %w", typeIdParts[1], err)
 	}
-
+	
 	return TypeId(typeId), nil
 }
 
@@ -67,15 +67,12 @@ func validateTypeName(typeName string) error {
 	if typeName == "" {
 		return fmt.Errorf("type name must not be empty")
 	}
-
 	if strings.HasPrefix(typeName, "_") || strings.HasSuffix(typeName, "_") {
 		return fmt.Errorf("type name must not start or end with an underscore")
 	}
-
-	typeNameRegex := regexp.MustCompile(`^[a-zA-Z0-9]+(?:_[a-zA-Z0-9]+)*$`)
+	typeNameRegex := regexp.MustCompile(`^[a-z0-9]+(?:_[a-z0-9]+)*$`)
 	if !typeNameRegex.MatchString(typeName) {
-		return fmt.Errorf("type name must only contain alphanumeric characters and single underscores (no special characters or spaces)")
+		return fmt.Errorf("type name must only contain lowercase alphanumeric characters and single underscores (no special characters or spaces)")
 	}
-
 	return nil
 }
